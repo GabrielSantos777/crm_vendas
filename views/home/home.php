83fce7209953d848fa7ce1617144be5aa2ca9d0d
GabrielSantos777 <gabrielsantos.erick12@gmail.com>
2025-04-24 23:04:30 -0300

    Fiz a adição de cols e estou evoluindo no home

--- views/home/home.php
+++ views/home/home.php
@@ -1,2 +1,34 @@
-
-
+<div class="section">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-md-6 col-xl-3 mb-2">
+                <div class="card">
+                    <div class="card-body">
+                        <h6 class="mb-2">Vendas no mês</h6>
+                        <h4 class="mb-3">R$ 999999,99</h4>
+                        <p class="mb-0">Você vendeu R${} neste mês
+                        </p>
+                    </div>
+                </div>
+            </div><div class="col-md-6 col-xl-3 mb-2">
+                <div class="card">
+                    <div class="card-body">
+                        <h6 class="mb-2">Pedidos no mês</h6>
+                        <h4 class="mb-3">99999999</h4>
+                        <p class="mb-0">Você teve ${} pedidos neste mês
+                        </p>
+                    </div>
+                </div>
+            </div><div class="col-md-6 col-xl-3 mb-2">
+                <div class="card">
+                    <div class="card-body">
+                        <h6 class="mb-2">Produto mais pedido</h6>
+                        <h4 class="mb-3">99999999</h4>
+                        <p class="mb-0">${} foi mais pedido neste mês
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
