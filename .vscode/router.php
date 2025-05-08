16ef92958c22a8218a75eb815081c62d844ace62
Gabriel Santos <gabrielsantos.erick12@gmail.com>
2025-05-08 17:00:04 -0300

    dados

--- .vscode/router.php
+++ .vscode/router.php
@@ -1,18 +1,13 @@
 <?php
-/* Hello dev {-_-}. Please not modify this file. Thank you. */
-
-if ($_SERVER['REQUEST_URI'] === '/favicon.ico') {
-    return false; // deixa o servidor tratar ou ignora
-}
+ /* Hello dev {-_-}. Please not modify this file. Thank you. */
 if (php_sapi_name() == "cli-server") {
-    class MyRouter extends \stdClass
-    {
+     class MyRouter extends \stdClass { 
         public function __construct()
         {
             $this->query = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
             $this->path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
             if (substr($this->path, -1) === "/") $this->path = substr($this->path, 0, -1);
-            if (strlen($this->path) > 0) $this->path = "$this->path";
+            if (strlen($this->path) > 0 ) $this->path = "$this->path";
             else if (empty($this->path)) $this->path = "/";
             if ($this->path === "/") $this->path = "/index.php";
         }
@@ -23,45 +18,30 @@ if (php_sapi_name() == "cli-server") {
         if (array_search(strtolower($pathinfo->extension), ["php", "html", "htm"]) !== false) {
             if (strtolower($_SERVER['REQUEST_URI']) === "/.vscode/router.php") die;
             ob_start();
-?>
+ ?>
             <script src="https://cdn.socket.io/4.7.5/socket.io.min.js" integrity="sha384-2huaZvOR9iDzHqslqwpR87isEmrfxqyWOF7hr7BY6KG0+hVKLoEXMPUJw3ynWuhO" crossorigin="anonymous"></script>
             <script type="module">
                 window.addEventListener('DOMContentLoaded', (event) => {
-                    const socket = io(`ws://localhost:5003`, {
-                        auth: {
-                            token: "ZGY2NDkzMGUtYjczZC00MzkxLTlkZGEtNTExMTZjZGI3MmJiMTc0NjcxMTM4NzIyNg==aMc"
-                        },
-                        withCredentials: true,
-                        reconnectionAttempts: 3
-                    });
-                    socket.on("connect", () => {
-                        console.log(`Connect_success : _${ socket.id }_live _php.`);
-                    });
-                    socket.on("message", data => {
-                        const d = data.toLowerCase() || "";
-                        if (!d || d.length === 0) return;
-                        if (d === 'refresh') {
-                            window.location.reload(true);
-                            return;
-                        };
-                        if (d.includes("/") && d.endsWith(".php")) {
-                            if (d === "/index.php") {
-                                window.location.href = "/";
-                                return;
-                            } else {
-                                window.location.href = d || "/";
-                                return;
-                            };
-                        }
-                    })
-                    socket.on("disconnect", (reason) => {
-                        console.log("Disconnected from server");
-                    });
+                const socket = io(`ws://localhost:5003`, { 
+                    auth: { token : "ODk0MTg1ZGYtZTBhMC00NDY2LWJjZDgtZmFiMGRmNzc5OWJhMTc0NjczMzMxODI0Mw==aMc" },   
+                    withCredentials: true,
+                    reconnectionAttempts: 3
                 });
+                socket.on("connect", () => { console.log(`Connect_success : _${ socket.id }_live _php.`); });
+                socket.on("message", data => {
+                    const d =  data.toLowerCase() || "";
+                    if(!d || d.length === 0) return;
+                    if(d === 'refresh') { window.location.reload(true); return ;};
+                    if(d.includes("/") && d.endsWith(".php")){
+                       if(d === "/index.php"){ window.location.href = "/"; return;}
+                       else { window.location.href = d || "/"; return ;};
+                    }
+                })
+                socket.on("disconnect", (reason) => {  console.log("Disconnected from server"); });
+                }); 
             </script>
-<?php ob_get_flush();
-        }
-    }
+        <?php  ob_get_flush(); }
+    }  
     include(str_replace("/", DIRECTORY_SEPARATOR,  __DIR__ . "/.." . $req->path));
 }
 ?>
